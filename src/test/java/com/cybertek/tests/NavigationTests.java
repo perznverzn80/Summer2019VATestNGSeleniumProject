e9cfb7fac6663415028db2fd100516f9dde33180
Parsa <pmehdi80@gmail.com>
2019-10-26 13:04:36 -0400

    8:14pm 10/25/2019

--- src/test/java/com/cybertek/tests/NavigationTests.java
+++ src/test/java/com/cybertek/tests/NavigationTests.java
@@ -14,13 +14,15 @@ public class NavigationTests {
         firefox("firefox");
     }
     public static void chrome(String browser){
-//hello
+
         WebDriver driver= WebDriverFactory.getDriver(browser);
         driver.get("https://www.google.com");
         String gTitle=driver.getTitle();
         driver.navigate().to("https://etsy.com");
         String eTitle1=driver.getTitle();
-        StringUtility.verifyEquals(gTitle,driver.getTitle());
+        //StringUtility.verifyEquals(gTitle,gTitle);
+        driver.navigate().back();
+        StringUtility.verifyEquals(gTitle,gTitle);
         driver.navigate().forward();
         String Title2=driver.getTitle();
         StringUtility.verifyEquals(eTitle1,Title2);
@@ -29,10 +31,23 @@ public class NavigationTests {
        }
        public static void firefox(String browser){
         WebDriver driver=WebDriverFactory.getDriver(browser);
+        driver.get("https://www.google.com");
+        String fTitle=driver.getTitle();
+        driver.navigate().to("https://etsy.com");
+        String feTitle=driver.getTitle();
+        driver.navigate().back();
+        StringUtility.verifyEquals(fTitle,fTitle);
+        driver.navigate().forward();
+        String fe2Title=driver.getTitle();
+        StringUtility.verifyEquals(feTitle,fe2Title);
         driver.quit();
+
        }
        public static void edge(String browser){
         WebDriver driver=WebDriverFactory.getDriver(browser);
+
+
+
         driver.quit();
        }
 
