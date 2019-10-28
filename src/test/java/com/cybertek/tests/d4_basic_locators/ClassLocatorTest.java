6dea38a6c53791fc1d5d2f56c883da460f5a564e
Parsa <pmehdi80@gmail.com>
2019-10-28 16:39:09 -0400

    8:14pm 10/25/2019

---
+++ src/test/java/com/cybertek/tests/d4_basic_locators/ClassLocatorTest.java
@@ -0,0 +1,28 @@
+package com.cybertek.tests.d4_basic_locators;
+
+import com.cybertek.tests.Utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ClassLocatorTest {
+
+    public static void main(String[] args) {
+
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/sign_up");
+        WebElement home=driver.findElement(By.className("nav-link"));
+        home.click();
+        driver.getTitle();
+        driver.navigate().to("http://practice.cybertekschool.com/");
+        WebElement title=driver.findElement(By.className("h3"));
+
+        System.out.println(title.getText());//type title and then dot sout and enter
+        driver.navigate().to("http://practice.cybertekschool.com/multiple_buttons");
+        WebElement multiplebotton=driver.findElement(By.className("btn btn-primary"));
+        multiplebotton.click();
+
+
+
+    }
+}
