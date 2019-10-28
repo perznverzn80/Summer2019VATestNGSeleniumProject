d033552e1929ca2a3975ecc6912227d5ffe6bdf8
Parsa <pmehdi80@gmail.com>
2019-10-28 14:53:58 -0400

    8:14pm 10/25/2019

---
+++ src/test/java/com/cybertek/tests/d4_basic_locators/IdLocatorTest.java
@@ -0,0 +1,22 @@
+package com.cybertek.tests.d4_basic_locators;
+
+import com.cybertek.tests.Utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class IdLocatorTest {
+    public static void main(String[] args) {
+
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/multiple_buttons");
+        driver.manage().window().maximize();
+        WebElement element=driver.findElement(By.id("disappearing_button"));
+        String txt=element.getText();
+        element.click();
+
+
+        driver.quit();
+
+    }
+}
